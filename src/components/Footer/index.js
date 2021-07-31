b7c983b865a28b8c04ec0f4bdc803b7b54887354
Rabiul Mazumder <rabiulmaz613@gmail.com>
2021-07-31 15:30:47 -0400

    init

---
+++ src/components/Footer/index.js
@@ -0,0 +1,91 @@
+import React from 'react'
+import { animateScroll as scroll} from 'react-scroll'
+import { FaFacebook, FaInstagram, FaLinkedin, FaTwitter, FaYoutube } from 'react-icons/fa'
+import {
+    FooterContainer,
+    FooterWrap,
+    FooterLinksContainer,
+    FooterLinksWrapper,
+    FooterLinkItems,
+    FooterLinkTitle,
+    FooterLink,
+    SocialMedia,
+    SocialMediaWrap,
+    SocialLogo,
+    WebsiteRights,
+    SocialIcons,
+    SocialIconLink
+} from './FooterElements'
+
+const Footer = () => {
+    const toggleHome = () => {
+        scroll.scrollToTop();
+    }
+
+    return (
+        <FooterContainer>
+            <FooterWrap>
+                <FooterLinksContainer>
+                    <FooterLinksWrapper>
+                        <FooterLinkItems>
+                            <FooterLinkTitle>About Us</FooterLinkTitle>
+                            <FooterLink to="/signin">How it works</FooterLink>
+                            <FooterLink to="/signin">Testimonials</FooterLink>
+                            <FooterLink to="/signin">Careers</FooterLink>
+                            <FooterLink to="/signin">Investors</FooterLink>
+                            <FooterLink to="/signin">Terms of Service</FooterLink>
+                        </FooterLinkItems>
+                        <FooterLinkItems>
+                            <FooterLinkTitle>Contact Us</FooterLinkTitle>
+                            <FooterLink to="/signin">Contact</FooterLink>
+                            <FooterLink to="/signin">Support</FooterLink>
+                            <FooterLink to="/signin">Destinations</FooterLink>
+                            <FooterLink to="/signin">Sponsorships</FooterLink>
+                        </FooterLinkItems>
+                    </FooterLinksWrapper>
+                    <FooterLinksWrapper>
+                        <FooterLinkItems>
+                            <FooterLinkTitle>Videos</FooterLinkTitle>
+                            <FooterLink to="/signin">Submit Videos</FooterLink>
+                            <FooterLink to="/signin">Ambassadors</FooterLink>
+                            <FooterLink to="/signin">Agency</FooterLink>
+                            <FooterLink to="/signin">Influncer</FooterLink>
+                        </FooterLinkItems>
+                        <FooterLinkItems>
+                            <FooterLinkTitle>Products and Services</FooterLinkTitle>
+                            <FooterLink to="/signin">Online Banking</FooterLink>
+                            <FooterLink to="/signin">Mobile Banking</FooterLink>
+                            <FooterLink to="/signin">Student Center</FooterLink>
+                            <FooterLink to="/signin">Accessible Banking</FooterLink>
+                        </FooterLinkItems>
+                    </FooterLinksWrapper>
+                </FooterLinksContainer>
+                <SocialMedia>
+                    <SocialMediaWrap>
+                        <SocialLogo to='/' onClick={toggleHome}>dolla</SocialLogo>
+                        <SocialIcons>
+                            <SocialIconLink href="//www.facebook.com/" target="_blank" aria-label="Facebook">
+                                <FaFacebook />
+                            </SocialIconLink>
+                            <SocialIconLink href="/www.instagram.com/" target="_blank" aria-label="Instagram">
+                                <FaInstagram />
+                            </SocialIconLink>
+                            <SocialIconLink href="//www.youtube.com/" target="_blank" aria-label="Youtube">
+                                <FaYoutube />
+                            </SocialIconLink>
+                            <SocialIconLink href="//www.twitter.com/" target="_blank" aria-label="Twitter">
+                                <FaTwitter />
+                            </SocialIconLink>
+                            <SocialIconLink href="//www.linkedin.com/" target="_blank" aria-label="Linkedin">
+                                <FaLinkedin />
+                            </SocialIconLink>
+                        </SocialIcons>
+                        <WebsiteRights>© {new Date().getFullYear()} dolla. All rights reserved.</WebsiteRights>
+                    </SocialMediaWrap>
+                </SocialMedia>
+            </FooterWrap>
+        </FooterContainer>
+    )
+}
+
+export default Footer

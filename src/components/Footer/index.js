dab1eb2a3306d1791dc322cf69cd34d28c84f047
Rabiul Mazumder <rabiulmaz613@gmail.com>
2021-08-01 18:04:44 -0400

    URL link error with Instagram

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -67,7 +67,7 @@ const Footer = () => {
                             <SocialIconLink href="//www.facebook.com/" target="_blank" aria-label="Facebook">
                                 <FaFacebook />
                             </SocialIconLink>
-                            <SocialIconLink href="/www.instagram.com/" target="_blank" aria-label="Instagram">
+                            <SocialIconLink href="//www.instagram.com/" target="_blank" aria-label="Instagram">
                                 <FaInstagram />
                             </SocialIconLink>
                             <SocialIconLink href="//www.youtube.com/" target="_blank" aria-label="Youtube">
